0184e31782f97c33df96084946b1b253f362663e
Ildar <galimzyanovi@mail.ru>
2022-08-01 17:08:11 +0300

    D8 done

---
+++ news1/sign/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .models import BaseRegisterForm
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class AuthorGroup(PermissionRequiredMixin, CreateView):
+    permission_required = ('sign.add_post', 'change.add_post')
+
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
